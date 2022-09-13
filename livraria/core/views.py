09c99d2016f92101d3a53c14f7b7884771bf3ee7
Leonardo Peixoto <106837819+LeonardoPeixotoBezerra@users.noreply.github.com>
2022-09-13 10:22:24 -0300

    Delete livraria directory

--- livraria/core/views.py
+++
@@ -1,9 +0,0 @@
-from urllib import request
-from django.http import HttpResponse
-
-
-def teste(request):
-    return HttpResponse("E o resto você já sabe")
-
-def teste2(request):
-    return HttpResponse("E o resto você já sabe 2.0")
