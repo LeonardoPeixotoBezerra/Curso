c659e540953839a9eeb952e0cf0d2d15da207baa
Leonardo Peixoto <106837819+LeonardoPeixotoBezerra@users.noreply.github.com>
2022-09-13 10:55:01 -0300

    Add files via upload

---
+++ livraria/core/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from core.models import Autor, Categoria, Compra, Editora, Livro
+
+admin.site.register(Autor)
+admin.site.register(Categoria)
+admin.site.register(Compra)
+admin.site.register(Editora)
+admin.site.register(Livro)
